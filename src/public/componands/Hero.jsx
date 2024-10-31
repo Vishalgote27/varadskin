5e956110ec9d47adb24292b2f3cb71075de9b008
vishal gote <vishal27@gmail.com>
2024-10-31 12:35:02 +0530

    vv

--- src/public/componands/Hero.jsx
+++ src/public/componands/Hero.jsx
@@ -24,13 +24,14 @@ const Hero = () => {
         </div> */}
 
 
-        <div className="relative w-full overflow-hidden mt-14">
+        <div className="banner-container mt-14">
             <img
                 src="/photos/newvaradskincare.jpg"
                 alt="Banner Image"
                 className="banner-image"
             />
         </div>
+
 
 
 
